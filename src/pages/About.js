3566a8e19750d56911f200c273916a4ef95381dd
endurance <enduremathew@gmail.com>
2023-04-20 22:40:11 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum error
+        aliquid, quia iusto molestiae deleniti reiciendis voluptatum sed sunt
+        iste inventore voluptates harum quam eveniet pariatur mollitia optio
+        eius quidem at excepturi nisi perferendis alias ex. Molestias,
+        aspernatur? Id praesentium similique sunt ipsa eius laboriosam odit
+        quidem qui ut quisquam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
